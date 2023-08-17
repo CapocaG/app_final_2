5b1dd4c2c9dcf074761ac2959f7355dbddf955d6
CapocaG <agabrielcar@gmail.com>
2023-08-16 20:06:59 -0500

    first commit

---
+++ src/test/java/com/example/app_final/AppFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.app_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
